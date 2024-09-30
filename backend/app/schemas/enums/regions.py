a0f6efdee83f386f42e43574bcd092beb8b1b8ca
Nikita <nikitabecker2003@gmail.com>
2024-09-30 22:46:08 +0300

    feat: Enums for UserProfile

---
+++ backend/app/schemas/enums/regions.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+class Regions(Enum):
+    NORTH = "HaZhaphon"
+    HAIFA = "Haifa"
+    TLV = "Tel-Aviv"
+    JSM = "Jerusalem"
+    MERKAZ = "HaMerkaz"
+    SOUTH = "HaDarom"
+    
+    
