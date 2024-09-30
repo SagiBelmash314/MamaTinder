696bed90d339123fa19a938a8f95224b946eba23
Nikita <nikitabecker2003@gmail.com>
2024-09-30 22:45:27 +0300

    feat: add UserProfile data model scheme

---
+++ backend/app/schemas/user_profile.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel
+from typing import List, Optional
+from enums import RelationshipGoal, Regions
+
+class UserProfile(BaseModel):
+    """
+    Defines the user profile model class.
+    - age : int > User's profile age.
+    - height : float > User's profile height.
+    - location : (optional) `Regions` > User's profile region location.
+    - relationship_goal : `RelationshipGoal` > User's profile relationship goal.
+    - about_me : (optional) string > User's profile "about me".
+    - pictures : List[string] > User's profile pictures.
+    - preferences: `UserPreferences` > User's profile preferences.
+    """
+    age: int
+    height: float
+    location: Optional[Regions] = None 
+    relationship_goal: RelationshipGoal
+    about_me: Optional[str] = None
+    pictures: List[str] = []
+    preference: UserPreferences
